a7900659d49ec6d674498c206466d171dd8119db
ahmedsaleh_93 <ahmed_saleh93@outlook.com>
2019-03-24 06:26:07 +0200

    adding Part2 files for Nodemcu"

--- Part1/3-node js/1-hello world/index.js
+++ Part1/3-node js/1-hello world/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+app.listen(PORT, function () {
+    console.log("Server listening on port " + PORT);
+})
 
 
-app.get('/ahmed', function (request, t) {
-	t.send("Hello World!");
+app.get('/ahmed', function (request, responde) {
+	responde.send("Hello ahmed!");
+	console.log(request);
+});
+app.get('/', function (request, responde) {
+	responde.send("Hello World!");
 	console.log(request);
 });
 
 
-app.listen(PORT, function () {
-    console.log("Server listening on port " + PORT);
-})
+
